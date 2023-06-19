ecbc757b538834f825a5543d26c52c68a7b63a29
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-19 12:22:58 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function createScheduleTables() {
+    var daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'];
+    var container = document.getElementById('scheduleTablesContainer');
+    
+    for (var i = 0; i < daysOfWeek.length; i++) {
+      var table = document.createElement('table');
+      table.id = 'scheduleTable_' + i;
+      table.innerHTML = '<tr><th colspan="3">' + daysOfWeek[i] + '</th></tr><tr><th>Время</th><th>Предмет</th><th>Преподаватель</th></tr><tr><td contenteditable="true"></td><td contenteditable="true"></td><td contenteditable="true"></td></tr>';
+      
+      container.appendChild(table);
+    }
+  }
