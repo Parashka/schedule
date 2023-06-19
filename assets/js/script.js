b7c7f5b2547b6a22fc854d65085b23553363d1bd
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-19 12:32:11 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
-createSchedule.getElementById("createSchedule");
-
-function createScheduleTables() {
+document.addEventListener("DOMContentLoaded", function() {
+    createScheduleTables();
+  });
+  
+  function createScheduleTables() {
     var daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница'];
     var container = document.getElementById('scheduleTablesContainer');
-    
+  
     for (var i = 0; i < daysOfWeek.length; i++) {
       var table = document.createElement('table');
       table.id = 'scheduleTable_' + i;
-      table.innerHTML = '<tr><th colspan="3">' + daysOfWeek[i] + '</th></tr><tr><th>Время</th><th>Предмет</th><th>Преподаватель</th></tr><tr><td contenteditable="true"></td><td contenteditable="true"></td><td contenteditable="true"></td></tr>';
-      
+      var tableContent = '<tr><th colspan="3">' + daysOfWeek[i] + '</th></tr><tr><th>Время</th><th>Предмет</th><th>Преподаватель</th></tr>';
+  
+      for (var j = 0; j < 5; j++) {
+        tableContent += '<tr><td contenteditable="true"></td><td contenteditable="true"></td><td contenteditable="true"></td></tr>';
+      }
+  
+      table.innerHTML = tableContent;
       container.appendChild(table);
-      createSchedule.disabled = true;
     }
-  }
+}
