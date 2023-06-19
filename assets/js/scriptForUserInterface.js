8d75255146410df3e29a98396544527f9d2d6d89
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-19 13:20:00 +0300

    1

--- assets/js/scriptForUserInterface.js
+++ assets/js/scriptForUserInterface.js
@@ -1,22 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var sourceTable = document.getElementById('scheduleTable_0'); 
-    var outputTable = document.getElementById('outputTable'); 
-  
-    if (sourceTable && outputTable) {
-      var sourceRows = sourceTable.getElementsByTagName('tr');
-  
-      for (var i = 1; i < sourceRows.length; i++) {
-        var sourceCells = sourceRows[i].getElementsByTagName('td');
-  
-        var newRow = document.createElement('tr');
-  
-        for (var j = 0; j < sourceCells.length; j++) {
-          var newCell = document.createElement('td');
-          newCell.textContent = sourceCells[j].textContent;
-          newRow.appendChild(newCell);
-        }
-  
-        outputTable.appendChild(newRow);
-      }
+    const viewTable = document.getElementById('viewTable');
+
+    // Загрузка данных из локального хранилища
+    const savedData = localStorage.getItem('tableData');
+    if (savedData) {
+      viewTable.innerHTML = savedData;
     }
   });
