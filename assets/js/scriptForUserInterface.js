ffb11b7024f0017525aef4eb8df36411b302a884
Parashka <72983243+Parashka@users.noreply.github.com>
2023-06-20 12:07:27 +0300

    creating YT Updates

--- assets/js/scriptForUserInterface.js
+++ assets/js/scriptForUserInterface.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const viewTable = document.getElementById('viewTable');
-
-    // Загрузка данных из локального хранилища
     const savedData = localStorage.getItem('tableData');
     if (savedData) {
       viewTable.innerHTML = savedData;
     }
   });
+
+
